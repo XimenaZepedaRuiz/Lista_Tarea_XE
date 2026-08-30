acfdaee94182ca4c026109bb07ebcce6a19392ae
XimenaZepedaRuiz <zepedax30@gmail.com>
2026-08-29 23:22:35 -0600

    Agrega estructura inicial de JavaScript

---
+++ app/script.js
@@ -0,0 +1,4 @@
+const tareaInput = document.getElementById("tareaInput");
+const agregarBtn = document.getElementById("agregarBtn");
+const listaTareas = document.getElementById("listaTareas");
+const limpiarBtn = document.getElementById("limpiarBtn");
