4dec7b4bf5f1b6b618d89b16af3e8e71079de94e
elizabeth05 <eidymartinez07@gmail.com>
2026-08-29 23:39:04 -0600

    Update script.js
    Implementa función para agregar tareas

--- app/script.js
+++ app/script.js
@@ -2,3 +2,21 @@ const tareaInput = document.getElementById("tareaInput");
 const agregarBtn = document.getElementById("agregarBtn");
 const listaTareas = document.getElementById("listaTareas");
 const limpiarBtn = document.getElementById("limpiarBtn");
+
+// Agregar una nueva tarea
+agregarBtn.addEventListener("click", function () {
+    const texto = tareaInput.value.trim();
+
+    if (texto === "") {
+        alert("Escribe una tarea antes de agregarla.");
+        return;
+    }
+
+    const nuevaTarea = document.createElement("li");
+
+    nuevaTarea.textContent = texto;
+
+    listaTareas.appendChild(nuevaTarea);
+
+    tareaInput.value = "";
+});
