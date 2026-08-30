cb42fc9333ff26b40bf3f600820af24878b25c95
elizabeth05 <eidymartinez07@gmail.com>
2026-08-30 00:15:19 -0600

    Implementa tareas completadas y limpieza

--- app/script.js
+++ app/script.js
@@ -14,9 +14,37 @@ agregarBtn.addEventListener("click", function () {
 
     const nuevaTarea = document.createElement("li");
 
-    nuevaTarea.textContent = texto;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const textoTarea = document.createElement("span");
+    textoTarea.textContent = texto;
+
+    checkbox.addEventListener("change", function () {
+        textoTarea.style.textDecoration = checkbox.checked
+            ? "line-through"
+            : "none";
+    });
+
+    nuevaTarea.appendChild(checkbox);
+    nuevaTarea.appendChild(textoTarea);
 
     listaTareas.appendChild(nuevaTarea);
 
     tareaInput.value = "";
 });
+
+// Limpiar tareas completadas
+limpiarBtn.addEventListener("click", function () {
+    const tareas = listaTareas.querySelectorAll("li");
+
+    tareas.forEach(function (tarea) {
+        const checkbox = tarea.querySelector("input");
+
+        if (checkbox.checked) {
+            tarea.remove();
+        }
+    });
+});
+
+ 
